1038a6e9ecf40d7ca4630fe525a02257f6e541f3
BozhenaOliinyk <bozhenaolijnyk01@gmail.com>
2026-06-23 11:15:40 +0300

    GPT-46 corrected directory name in ssh.sh

--- bozhena_oliinyk/ssh.sh
+++ bozhena_oliinyk/ssh.sh
@@ -2,25 +2,25 @@ cat << 'EOF' > ~/.ssh/config
 Host web1 192.168.56.101
     HostName 192.168.56.101
     User vagrant
-    IdentityFile "D:/vagrant_project/bird_watching_iac/.vagrant/machines/web1/virtualbox/private_key"
+    IdentityFile "D:/vagrant_project/bird_watching_iac/bozhena_oliinyk/.vagrant/machines/web1/virtualbox/private_key"
     StrictHostKeyChecking no
     UserKnownHostsFile /dev/null
 Host web2 192.168.56.102
     HostName 192.168.56.102
     User vagrant
-    IdentityFile "D:/vagrant_project/bird_watching_iac/.vagrant/machines/web2/virtualbox/private_key"
+    IdentityFile "D:/vagrant_project/bird_watching_iac/bozhena_oliinyk/.vagrant/machines/web2/virtualbox/private_key"
     StrictHostKeyChecking no
     UserKnownHostsFile /dev/null
 Host nginx 192.168.56.103
     HostName 192.168.56.103
     User vagrant
-    IdentityFile "D:/vagrant_project/bird_watching_iac/.vagrant/machines/nginx/virtualbox/private_key"
+    IdentityFile "D:/vagrant_project/bird_watching_iac/bozhena_oliinyk/.vagrant/machines/nginx/virtualbox/private_key"
     StrictHostKeyChecking no
     UserKnownHostsFile /dev/null
 Host db 192.168.56.104
     HostName 192.168.56.104
     User vagrant
-    IdentityFile "D:/vagrant_project/bird_watching_iac/.vagrant/machines/db/virtualbox/private_key"
+    IdentityFile "D:/vagrant_project/bird_watching_iac/bozhena_oliinyk/.vagrant/machines/db/virtualbox/private_key"
     StrictHostKeyChecking no
     UserKnownHostsFile /dev/null
 EOF
